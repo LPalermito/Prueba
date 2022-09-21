f85c449f2c9e0e434a93d73d439b8130ae9ddd8d
lPalermito <lpalermo417@gmail.com>
2022-09-21 17:31:42 -0300

    segunda version

--- funciones.js
+++ funciones.js
@@ -2,3 +2,11 @@ function despedir(){
     console.log("chauu");
 
 }
+
+
+function saludar (nombre, apellido)   {
+    console.log ("Hola " + nombre + " " + apellido ".");
+    var divSaludar = document.getElementById("ElPrimerDiv");
+    divSaludar.innerText = "Hola"
+    alert("Hola " + nombre + " " + apellido ".");
+}
